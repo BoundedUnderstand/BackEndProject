5dafb737bd53f135c7babc7383e7b24d14190d1d
BoundedUnderstand <patrykbu@wp.pl>
2025-06-12 17:52:11 +0200

    Dodaj pliki projektów.

---
+++ ApplicationCore/Models/Subscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class Subscription
+    {
+        [Key]
+        public int customerId { get; set; }
+        public DateTime createdDate { get; set; }
+        
+        public DateTime? canceledDate { get; set; }
+        
+        public int subscriptionCost { get; set; }
+
+        public string subscriptionInterval { get; set; } 
+
+        public bool wassubscriptionPaid { get; set; }
+    }
+}
---
+++ Infrastructure/EF/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.EF;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        /*var config =*/ 
+            /*new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+             .AddEnvironmentVariables()
+             .Build();*/
+
+
+
+        var defaultConn = "Host=localhost;Port=5432;Database=moja_baza;Username=postgres;Password=moje_haslo";
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? defaultConn;
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+
+    }
+}
